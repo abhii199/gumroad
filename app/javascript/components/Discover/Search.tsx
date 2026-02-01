abhii199/gumroad

commit be281f19133aa73a18fcee47b5b7c1198362eb07

Migrate `_forms.scss` to Tailwind: Part 1 (#2942)
Issue: #1055

Splitting #2747 into micro-PRs as discussed in
https://github.com/antiwork/gumroad/pull/2747#issuecomment-3762858746

# Description
- Migrate core form components and admin pages to Tailwind

#### Note: To test the UI changes in this PR locally, please make sure
to first comment out the `_forms.scss` then restart the dev server.
# Demo
## Before

https://private-user-images.githubusercontent.com/145349710/536926660-8e82afe9-55cd-494f-9f52-53623c78d3b6.mov?jwt=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3Njg2NDI1MjIsIm5iZiI6MTc2ODY0MjIyMiwicGF0aCI6Ii8xNDUzNDk3MTAvNTM2OTI2NjYwLThlODJhZmU5LTU1Y2QtNDk0Zi05ZjUyLTUzNjIzYzc4ZDNiNi5tb3Y_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjYwMTE3JTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI2MDExN1QwOTMwMjJaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT1kMzE4OTEzY2FiZDUzZDMwYWY5ZTA5Y2E0MmExOTE4NmY1OTQ5NGZmYzJlN2EwMjFmNTE2Y2M1NzAzMThkZTIwJlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCJ9.ibtjzX4PAz2Ri1ASytv1dFnjCKan1WWY1uI2lmYFwMY

## After

https://private-user-images.githubusercontent.com/145349710/536072985-2e040acb-1dfb-40a2-94ea-afcf31a2a7b1.mov?jwt=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3Njg2NDI1MjIsIm5iZiI6MTc2ODY0MjIyMiwicGF0aCI6Ii8xNDUzNDk3MTAvNTM2MDcyOTg1LTJlMDQwYWNiLTFkZmItNDBhMi05NGVhLWFmY2YzMWEyYTdiMS5tb3Y_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjYwMTE3JTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI2MDExN1QwOTMwMjJaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT0yYjAwNmJhNWVkNmJhZWM5MzZmZWJhZDQ1ZGM4NGU3ZTYxOWEzYzc4ODM2ZjgzMjJjOWI4ZTdkOGI3MzlhMDIyJlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCJ9.mX6ZNbgzNsWA23jdl0VKmSffH-pIt3AzrJAr1qiz9Xc

---

app/javascript/components/Discover/Search.tsx
@@ -108,7 +108,11 @@ export const Search = ({ query, setQuery }: { query?: string | undefined; setQue
             </div>
           ) : (
             <a {...props} href={item.url} className={cx("flex items-center gap-4 no-underline", props.className)}>
-              <img src={item.thumbnail_url ?? thumbnailPlaceholder} alt={item.name} />
+              <img
+                src={item.thumbnail_url ?? thumbnailPlaceholder}
+                alt={item.name}
+                className="h-12 w-12 flex-none rounded border border-border object-cover"
+              />
               <div>
                 {highlightQuery(item.name)}
                 <small>{item.seller_name ? `Product by ${item.seller_name}` : "Product"}</small>

commit be7aaaa40ec3c0cb664083661aa3ce2e1a88d31a

Fix Discover search section padding (#3294)
Fixes a minor regression from #2942 - in the Discover search
autocomplete the "Products" title was missing padding.

app/javascript/components/Discover/Search.tsx
@@ -1,7 +1,7 @@
 import cx from "classnames";
 import * as React from "react";
 
-import { getAutocompleteSearchResults, AutocompleteSearchResults, deleteAutocompleteSearch } from "$app/data/discover";
+import { AutocompleteSearchResults, deleteAutocompleteSearch, getAutocompleteSearchResults } from "$app/data/discover";
 import { escapeRegExp } from "$app/utils";
 import { asyncVoid } from "$app/utils/promise";
 import { assertResponseError } from "$app/utils/request";
@@ -94,7 +94,9 @@ export const Search = ({ query, setQuery }: { query?: string | undefined; setQue
       option={(item, props, index) => (
         <>
           {index === results?.recent_searches.length ? (
-            <h3>{enteredQuery ? "Products" : results.viewed ? "Keep shopping for" : "Trending"}</h3>
+            <h3 className="px-4 py-2">
+              {enteredQuery ? "Products" : results.viewed ? "Keep shopping for" : "Trending"}
+            </h3>
           ) : null}
           {typeof item === "string" ? (
             <div {...props}>